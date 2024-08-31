d984feb172f104af9d360d627f3ca2f46a6b1e44
wenzel audrin barboza <wenzelbarboza@gmail.com>
2024-08-31 10:19:43 +0300

    build script added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -13,10 +13,11 @@ app.use(express.static("public"));
 //routes imports
 import { userRouter } from "./routes/user.routes";
 import { globalCatch } from "./utils/globalCatch";
+import { asyncHandler } from "./utils/asyncHandler";
 
 app.get("/", (req, res) => {
   return res.status(200).json({
-    message: "sir you have arrived",
+    message: "welcome to JungleJobs backend",
   });
 });
 
