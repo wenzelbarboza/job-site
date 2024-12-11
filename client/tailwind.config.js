2ad2018f586a7a1cac2f62462b008464c59a19ee
wenzel audrin barboza <wenzelbarboza@gmail.com>
2024-12-11 15:58:01 +0200

    import error fix in tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -73,5 +74,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
