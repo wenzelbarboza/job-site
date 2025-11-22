9525ed4c5bebf6e4c8c668c71369f7f5613247fc
wenzel audrin barboza <wenzelbarboza@gmail.com>
2025-11-22 09:24:50 +0530

    fix type error or req.user

--- server/src/utils/types.ts
+++ server/src/utils/types.ts
@@ -2,12 +2,6 @@ import { JwtPayload } from "jsonwebtoken";
 import { refreshType } from "../models/user.models";
 import { z } from "zod";
 
-declare module "express-serve-static-core" {
-  interface Request {
-    user?: number;
-  }
-}
-
 export type MyJwtPayload = JwtPayload & {
   userId: number;
 };
